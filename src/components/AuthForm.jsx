3c3076094e427b0b7b5384c18c7e23e20956be91
Louise Ly <ly.louise07@gmail.com>
2023-03-04 15:05:31 -0600

    Finished basic homepage

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -6,20 +6,41 @@ const AuthForm = ({ action }) => {
   const [password, setPassword] = useState('');
 
   return (
-    <form className="w-2/5 flex flex-col">
-      {action === 'signup' && (
-        <>
-          <label htmlFor="name">Name</label>
-          <input type="text" value={name} className="rounded-lg" />
-        </>
-      )}
-      <label htmlFor="email">Email</label>
-      <input type="text" value={email} className="rounded-lg" />
-      <label htmlFor="password">Password</label>
-      <input type="password" value={password} className="rounded-lg" />
-      <div className="flex flex-col">
-        <button className="btn btn-primary">{action === 'signup' ? 'Create Account' : 'Sign In'}</button>
-        <button className="btn btn-secondary">{action === 'signup' ? 'Sign Up' : 'Sign In'} With Google</button>
+    <form className="flex flex-col justify-center border bg-[#737373] h-fit py-10 px-8 rounded-lg">
+      <div className="flex flex-col space-y-2">
+        {action === 'signup' && (
+          <>
+            <label htmlFor="name" className="text-gray-200">Name</label>
+            <input
+              className="rounded-lg py-2 px-3 focus:outline-none"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </>
+        )}
+        <label htmlFor="email" className="text-gray-200">Email</label>
+        <input
+          className="rounded-lg py-2 px-3 focus:outline-none"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password" className="text-gray-200">Password</label>
+        <input
+          className="rounded-lg py-2 px-3 focus:outline-none"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-col mt-10 space-y-4">
+        <button className="btn btn-primary normal-case text-base font-medium">
+          {action === 'signup' ? 'Create Account' : 'Sign In'}
+        </button>
+        <button className="btn btn-secondary normal-case text-base font-medium text-gray-500">
+          {action === 'signup' ? 'Sign Up' : 'Sign In'} With Google
+        </button>
       </div>
     </form>
   );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
-const Home = () => {
 
+const Home = () => {
   return (
-    <div className="container flex space-x-12">
+    <div className="container flex space-x-12 items-center">
       <div className="relative">
-        <Link to='/' className="absolute top-4 left-4 border border-0 px-12 py-2 bg-[#7e22ce] text-white">Diggr</Link>
+        <Link
+          to="/"
+          className="absolute top-4 left-4 border border-0 px-12 py-2 bg-[#7e22ce] text-white"
+        >
+          Diggr
+        </Link>
         <img
           className="h-screen"
           src="https://images.unsplash.com/photo-1591608971358-f93643d11763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
           alt="dog-image"
         />
       </div>
-      <AuthForm action="signup" />
+      <div className="w-2/5 flex flex-col space-y-5">
+        <h3 className="text-2xl text-center text-gray-800">Create An Account</h3>
+        <AuthForm action="signup" />
+        <p className="text-center">
+          Already Have An Account? &nbsp;
+          <Link to="/login" className="font-bold">
+            Sign In
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
