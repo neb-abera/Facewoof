615a60253366d7e508e46793c88d4d2a2bb7a7b3
Neb Abera <29741322+neb-abera@users.noreply.github.com>
2023-03-03 21:39:19 -0600

    rename login to match naming convention
    Signed-off-by: Nebyou Abera <nebyouabera@gmail.com>

---
+++ src/views/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect, useContext, useRef } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+      <button type="button">Go fetch</button>
+    </div>
+  );
+};
+
+export default Login;
