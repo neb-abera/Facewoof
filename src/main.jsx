9a54e5923a5817a0e4ca65dbaa273bd85f2e75de
Louise Ly <ly.louise07@gmail.com>
2023-03-04 11:30:00 -0600

    Changed App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Home from './views/Home';
-import Login from './views/Login';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        {/* <App /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
