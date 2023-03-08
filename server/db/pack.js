e0a4226466e88ceffc9a801009e0cc6032ffcd26
Gabe <gbennettbrandt@gmail.com>
2023-03-08 16:21:54 -0600

    Updating draft pack query

--- server/db/pack.js
+++ server/db/pack.js
@@ -3,16 +3,23 @@
 const db = require('./database');
 
 // Add a user to a pack if not already in that pack
-function addToPack(user, pack) {
-  return db.query(`
+function addToPack(user_id, pack_id) {
+  return db
+    .query(
+      `
     INSERT INTO pack_users ("pack_id", "user_id")
-    SELECT ${user.user_id}, ${pack.pack_id}
+    SELECT ${user_id}, ${pack_id}
     WHERE
       NOT EXISTS (
-      SELECT user_id FROM pack_users WHERE pack_id = ${pack.pack_id} AND user_id = ${user.user_id}
+      SELECT user_id FROM pack_users WHERE pack_id = ${pack_id} AND user_id = ${user_id}
     )
-  `)
-  .catch((err) => {
-    console.log('Error adding to packs:', err);
-  });
+  `
+    )
+    .catch((err) => {
+      console.log('Error adding to packs:', err);
+    });
 }
+
+module.exports = {
+  addToPack,
+};
