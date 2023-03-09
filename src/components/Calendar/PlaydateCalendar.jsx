f97c379371499a296a31cd644612d51363077ba1
ctunakan <clairetunakan@gmail.com>
2023-03-09 15:29:32 -0600

    Fixed bug

--- src/components/Calendar/PlaydateCalendar.jsx
+++ src/components/Calendar/PlaydateCalendar.jsx
@@ -24,7 +24,7 @@ const PlaydateCalendar = ({
   const [showPlaydateModal, setShowPlaydateModal] = useState(false);
   const [eventsData, setEventsData] = useState([]);
 
-  const { playdates /*, setPlaydates, handleSetPlaydates*/ } = useUserContext();
+  const { playdates, setPlaydates, handleSetPlaydates } = useUserContext();
 
   useEffect(() => {
     setEventsData(playdates);
