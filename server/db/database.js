4e7eb4d0d21b26f75d58ece736d3872169aa450d
Gabe Bennett-Brandt <gbennettbrandt@gmail.com>
2023-03-10 09:37:13 -0600

    Update database.js
    Signed-off-by: Gabe Bennett-Brandt <gbennettbrandt@gmail.com>

--- server/db/database.js
+++ server/db/database.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
-console.log('process.env.PGUSERNAME', process.env.PGUSERNAME);
+// console.log('process.env.PGUSERNAME', process.env.PGUSERNAME);
 const db = new Client({
   user: process.env.PGUSERNAME,
   host: process.env.PGHOST,
