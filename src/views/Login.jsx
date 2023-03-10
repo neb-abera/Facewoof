131ed66d77927aaf3fe41c16dc3c1ffb7348b344
Neb Abera <29741322+neb-abera@users.noreply.github.com>
2023-03-10 09:29:39 -0600

    created demo and production versions of setting user info

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -36,11 +36,42 @@ const Login = ({ config }) => {
         .getUser()
         .then((info) => {
           setLoggedIn(true);
-          // uncomment this section to activate demo using user 7
-          // axios
-          //   .put('/api/authuser', { email: 'pyekel6@marketwatch.com', name: 'Abdel Dandie' })
-          //   .then((res) => {
-          //     console.log('response from db query', res.data);
+
+          // uncomment this axios request section to activate demo using user 7
+          axios
+            .put('/api/authuser', { email: 'pyekel6@marketwatch.com', name: 'Abdel Dandie' })
+            .then((res) => {
+              // console.log('response from db query', res.data);
+              const user = {
+                user_id: res.data.user_id,
+                dog_name: res.data.dog_name,
+                owner_name: res.data.owner_name,
+                owner_first_name: info.given_name,
+                owner_last_name: info.family_name,
+                dog_breed: res.data.dog_breed,
+                age: res.data.age,
+                vaccination: res.data.vaccination,
+                discoverable: res.data.discoverable,
+                owner_email: res.data.owner_email,
+                location: res.data.location,
+                likes_one: res.data.likes_one,
+                likes_two: res.data.likes_two,
+                likes_three: res.data.likes_three
+              };
+              setUserId(res.data.user_id);
+              setUserData(user);
+              setFirstLogin(false);
+              history.push('/discover');
+            });
+
+          // uncomment the below to activate production (user creation and redirect to signup page)
+          // const name = `${info.given_name} ${info.family_name}`;
+          // // check if there is a user with a matching email
+          // axios.put('/api/authuser', { email: info.email, name: name }).then((res) => {
+          //   if (res.data.age !== null) {
+          //     // need to verify what returns to indicate user exists and if it contains correct data
+          //     setFirstLogin(false);
+          //    // console.log('response from db query', res.data);
           //     setUserId(res.data.user_id);
           //     const user = {
           //       user_id: res.data.user_id,
@@ -58,54 +89,23 @@ const Login = ({ config }) => {
           //       likes_two: res.data.likes_two,
           //       likes_three: res.data.likes_three
           //     };
+          //     setUserData(user);
+          //     history.push('/discover');
+          //   } else {
+          //     // if user hasn't filled signup form, redirect to signup page
+          //     setFirstLogin(true);
+          //     const user = {
+          //       user_id: res.data.user_id,
+          //       owner_name: name,
+          //       owner_first_name: info.given_name,
+          //       owner_last_name: info.family_name,
+          //       owner_email: info.email
+          //     };
           //     setUserId(res.data.user_id);
           //     setUserData(user);
-          //     setFirstLogin(false);
-          //     history.push('/discover');
-          //   });
-
-          // uncomment the below to activate user creation and redirect to signup page
-          const name = `${info.given_name} ${info.family_name}`;
-          // check if there is a user with a matching email
-          axios.put('/api/authuser', { email: info.email, name: name }).then((res) => {
-            if (res.data.age !== null) {
-              // need to verify what returns to indicate user exists and if it contains correct data
-              setFirstLogin(false);
-              console.log('response from db query', res.data);
-              setUserId(res.data.user_id);
-              const user = {
-                user_id: res.data.user_id,
-                dog_name: res.data.dog_name,
-                owner_name: res.data.owner_name,
-                owner_first_name: info.given_name,
-                owner_last_name: info.family_name,
-                dog_breed: res.data.dog_breed,
-                age: res.data.age,
-                vaccination: res.data.vaccination,
-                discoverable: res.data.discoverable,
-                owner_email: res.data.owner_email,
-                location: res.data.location,
-                likes_one: res.data.likes_one,
-                likes_two: res.data.likes_two,
-                likes_three: res.data.likes_three
-              };
-              setUserData(user);
-              history.push('/discover');
-            } else {
-              // if user hasn't filled signup form, redirect to signup page
-              setFirstLogin(true);
-              const user = {
-                user_id: res.data.user_id,
-                owner_name: name,
-                owner_first_name: info.given_name,
-                owner_last_name: info.family_name,
-                owner_email: info.email
-              };
-              setUserId(res.data.user_id);
-              setUserData(user);
-              history.push('/signup');
-            }
-          });
+          //     history.push('/signup');
+          //   }
+          // });
         })
         .catch((err) => {
           console.error(err);
